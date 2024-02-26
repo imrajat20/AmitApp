d25f2f6796ca06605d3dfcc3a73546db36448f7c
imrajat20 <yaduvanshiamit3@gmail.com>
2024-02-26 20:53:57 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <label for='epense'></label>
+            <h2 id='epense'>Expense Items</h2>
+            <label for='food'></label>
+            <h2 id='food'>Food Rs 10</h2>
+            <label for='petrol'></label>
+            <h2 id='petrol'>Petrol Rs 100</h2>
+            <label for='movie'></label>
+            <h2 id='movie'>Movies Rs 200</h2>
+
+        </div>
+    );
+}
+
+export default ExpenseItem;
